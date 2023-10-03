a377c7ad16fc49396f31e3373bda96ee103d9ec1
Chopppaa <146105882+Chopppaa@users.noreply.github.com>
2023-10-03 16:40:56 +0000

    lab_1

---
+++ lab_1_1task.py
@@ -0,0 +1,7 @@
+print('Hello, World!')
+print('Маша + Петя = Любовь')
+print('x=3 + 4')
+a=3
+b=4
+X = a+b
+print(X)
---
+++ lec_1_input.py
@@ -0,0 +1,11 @@
+a=input()
+print(a)
+
+a=input('vvedite znachenie: ')
+print(a)
+
+print(type(a))
+
+a = int(input('vvedite a: '))
+print(a)
+print(type(a))
